317ca97963e794617d396773a90111d0ee74387f
Adams <apptechng2@gmail.com>
2025-07-09 20:08:33 +0100

    UI update

---
+++ clients/src/components/ConnectedAccount.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+const ConnectedAccount = ({account}) => {
+
+    
+
+
+  return (
+    <div className="account">Connected Account: {account}</div>
+  )
+}
+
+export default ConnectedAccount
